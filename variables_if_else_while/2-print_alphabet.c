0be4cd38d32094288ee74aca328014eb563741a8
Maxime Font <maximefont@The-Source.local>
2025-02-19 12:18:37 +0100

    hooray

---
+++ variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,22 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+
+/**
+ * main - print alphabet in two uses of putchar and a for loop. wish me luck
+ * return: always (0) (success)
+ */
+
+int main(void)
+{
+	char letter = 'a';
+
+	while (letter <= 'z')
+	{
+	putchar(letter);
+	letter++;
+	}
+
+	putchar('\n');
+	return (0);
+}
