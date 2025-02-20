589945c261329e1aaf92b67e870777dce4e36ac2
Maxime Font <maximefont@The-Source.local>
2025-02-20 13:08:01 +0100

    LEGEN-

---
+++ variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/**
+ * main - series of soviet experiments on how char and putchar work*
+ * Return: always 0
+ */
+
+int main(void)
+{
+	char rettel = 'z';
+
+	while (rettel >= 'a')
+	{
+		putchar(rettel);
+		rettel--;
+	}
+	putchar('\n');
+	return (0);
+}
