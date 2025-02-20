efddd4d5956f3017d93c475343667e9fa89e7cf9
Maxime Font <maximefont@The-Source.local>
2025-02-20 11:09:49 +0100

    woohoohoo

---
+++ variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+/**
+ * main - boyoyoyowowowoo
+ *
+ * Return: always 0 (success)
+ */
+
+int main(void)
+{
+	char numb = '0';
+
+	while (numb <= '9')
+		{
+		putchar(numb);
+		numb++;
+		}
+	putchar('\n');
+	return (0);
+}
