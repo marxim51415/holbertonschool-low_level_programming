14d329222f337eb8911476421a46329974988252
Maxime Font <maximefont@The-Source.local>
2025-02-20 17:50:34 +0100

    tired

--- variables_if_else_while/9-print_comb.c
+++ variables_if_else_while/9-print_comb.c
@@ -3,20 +3,23 @@
 /**
  * main - dronka bronk
  *
- * Return: always 0
+ * Return: 0
  */
 
 int main(void)
 {
 	int num = 0;
 
-	while (num <= 8)
+	while (num <= 9)
 	{
 		putchar(num + '0');
+		if (num < 9)
+		{
 		putchar(',');
 		putchar(' ');
 		num++;
+		}
 	}
-	putchar(num + '0');
+	putchar('\n');
 	return (0);
 }
