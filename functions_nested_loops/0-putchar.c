ca4cdac83ed8ec858915cec8d705fc269d6c245c
Maxime Font <maximefont@The-Source.local>
2025-02-21 11:51:28 +0100

    somethings fishy here

---
+++ functions_nested_loops/0-putchar.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+/**
+* main - prints "_putchar" and that's it
+* Return: always 0
+*/
+
+int main(void)
+{
+	printf("_putchar\n");
+	return (0);
+}
---
+++ functions_nested_loops/README.md
@@ -0,0 +1,1 @@
+this README.md is not empty
