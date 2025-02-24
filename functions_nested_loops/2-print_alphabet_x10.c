4329bc2f56ca011ea687bdbc19713d1f8d6b054c
Maxime Font <maximefont@The-Source.local>
2025-02-24 11:54:00 +0100

    are we so back?

---
+++ functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+* main - prints alphabet 10 times
+* Return: always 0
+*/
+
+void print_alphabet_x10(void)
+{
+	char letter;
+	int i = '0';
+
+	letter = 'a';
+
+	for (i = 0 ; i <= 9 ; i++)
+	{
+		while (letter <= 'z')
+		{
+		_putchar(letter);
+		letter++;
+		}
+	_putchar('\n');
+	}
+}
