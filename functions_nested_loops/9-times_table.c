53abd009a74b3cbc7c295a2637148a265861f282
Maxime Font <maximefont@The-Source.local>
2025-02-24 17:52:37 +0100

    we did it boys

--- functions_nested_loops/9-times_table.c
+++ functions_nested_loops/9-times_table.c
@@ -7,11 +7,38 @@
 
 void times_table(void)
 {
-	int nine;
+	int nine = 0;
+	int times = 0;
+	int product;
 
 	for (nine = 0 ; nine <= 9 ; nine++)
 	{
-		_putchar((nine * 9) + '0');
-		_putchar('\n');
+		for (times = 0 ; times <= 9 ; times++)
+		{
+			product = nine * times;
+
+			if (product == 0)
+			{
+			_putchar('0' + product);
+			}
+
+			else if (product <= 9)
+			{
+			_putchar(',');
+			_putchar(' ');
+			_putchar('0' + product);
+			}
+
+			else
+			{
+			_putchar(',');
+			_putchar(' ');
+			_putchar('0' + (product / 10));
+			_putchar('0' + (product % 10));
+			}
+		}
+
+	_putchar('\n');
+	times = 0;
 	}
 }
