1aa8899ef302cf71ac8c869b78985e0bad0c3101
Maxime Font <maximefont@The-Source.local>
2025-02-24 14:04:13 +0100

    m16

---
+++ functions_nested_loops/6-abs.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * _abs - prints absolute value of integer n
+ * Return: always 0 (success)
+ */
+
+int _abs(int n)
+{
+	unsigned char myChar;
+
+	myChar = unsigned int(n);
+
+	_putchar(myChar);
+	return (0);
+}
