ee4adaad3afa4344c2e6dc0f97117e388b7f1eeb
Maxime Font <maximefont@The-Source.local>
2025-02-24 11:20:54 +0100

    why must we suffer

---
+++ functions_nested_loops/1-alphabet.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+* print_alphabet - putchar alphabet in lowercase
+*/
+
+void print_alphabet(void)
+{
+	char letter;
+
+	letter = 'a';
+
+	while (letter <= 'z')
+	{
+		_putchar(letter);
+		letter++;
+	}
+	_putchar('\n');
+}
